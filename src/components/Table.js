31785b01640f7163a2c792973864775407b0c6a8
leonardobrindeiro <leonardo_brindeiro@yahoo.com.br>
2022-10-21 21:38:32 -0300

    requisito 3 e 4

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,7 +2,33 @@ import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
 
 function Table() {
-  const { data, name, handlechange } = useContext(AppContext);
+  const { data,
+    handleNumb,
+    handleclick,
+    numb,
+    filtes,
+    name,
+    handlechange,
+    selectionChange,
+    selectionMaior } = useContext(AppContext);
+  // ajuda do andre horman e julio severo
+  const handlefilter = (planets) => {
+    let planetaFilt = planets;
+    filtes.forEach(({ column, maior, numbe }) => {
+      if (maior === 'maior que') {
+        planetaFilt = planetaFilt.filter((planeta) => (
+          Number(planeta[column]) > Number(numbe)));
+      } else if (maior === 'menor que') {
+        planetaFilt = planetaFilt.filter((planeta) => (
+          Number(planeta[column]) < Number(numbe)));
+      } else {
+        planetaFilt = planetaFilt.filter((planeta) => (
+          Number(planeta[column]) === Number(numbe)));
+      }
+    });
+    return planetaFilt;
+  };
+
   return (
     <div>
       <h1>Projeto Star Wars - Trybe</h1>
@@ -13,29 +39,34 @@ function Table() {
         onChange={ handlechange } // não posso esquecer do meu handlechange para mudança de estado
       />
       <select
-        data-testid="column-filter" // mesmo modelo de código do projeto tryunfo
+        data-testid="column-filter"
+        onChange={ selectionChange } // mesmo modelo de código do projeto tryunfo
       >
         <option value="population">population</option>
-        <option value="orbital_period"> orbital_period</option>
+        <option value="orbital_period">orbital_period</option>
         <option value="diameter">diameter</option>
         <option value="rotation_period">rotation_period</option>
         <option value="surface_water">surface_water</option>
       </select>
       <select
-        data-testid="comparison-filter" // mesmo modelo de código do projeto tryunfo
+        data-testid="comparison-filter"
+        onChange={ selectionMaior }// mesmo modelo de código do projeto tryunfo
       >
         <option value="maior que">maior que</option>
-        <option value="menor que"> menor que</option>
+        <option value="menor que">menor que</option>
         <option value="igual a">igual a</option>
+
       </select>
       <input
         type="number"
         data-testid="value-filter"
+        value={ numb }
+        onChange={ handleNumb }
       />
       <button
         type="button"
         data-testid="button-filter"
-
+        onClick={ handleclick }
       >
         Filtrar
       </button>
@@ -55,7 +86,7 @@ function Table() {
           <th>edited</th>
           <th>url</th>
         </tr>
-        {data?.filter((el) => el.name.includes(name)).map((p) => (
+        {handlefilter(data)?.filter((el) => el.name.includes(name)).map((p) => (
           <tr key={ p.name }>
             <td>{p.name}</td>
             <td>{p.rotation_period}</td>
--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState, useMemo, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 import fetchData from '../service/fetchApi';
@@ -6,13 +6,27 @@ import fetchData from '../service/fetchApi';
 function AppProvider({ children }) {
   const [data, setData] = useState([]);
   const [name, setName] = useState('');
-  const [column, setColumn] = useState('');
+  const [column, setColumn] = useState('population');
+  const [maior, setMaior] = useState('maior que');
+  const [numb, setNumb] = useState('0');
+  const [filtes, setFiltes] = useState([]);
+
   const handlechange = ({ target }) => {
     setName(target.value);
   };
   const selectionChange = ({ target }) => {
     setColumn(target.value);
   };
+  const selectionMaior = ({ target }) => {
+    setMaior(target.value);
+  };
+  const handleNumb = ({ target }) => {
+    setNumb(target.value);
+  };
+  const handleclick = useCallback(() => {
+    console.log({ column, maior, numb });
+    setFiltes([...filtes, { column, maior, numbe: numb }]);
+  }, [column, filtes, maior, numb]);
 
   useEffect(() => {
     const requestAPI = async () => {
@@ -26,9 +40,15 @@ function AppProvider({ children }) {
     data,
     name,
     column,
+    maior,
+    numb,
+    filtes,
+    handleclick,
     handlechange,
     selectionChange,
-  }), [data, name, column]);
+    selectionMaior,
+    handleNumb,
+  }), [data, name, column, maior, numb, filtes, handleclick]);
 
   return (
     <AppContext.Provider value={ contexto }>
